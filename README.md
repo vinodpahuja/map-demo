03c2f16e2316c613c01452ce3587de664d6da77e
Vinod Pahuja <vinod.pahuja@gmail.com>
2023-06-05 18:42:05 +0530

    Google Map JS WebGL API - Demo 2

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Demo applications for maps
 
 -  [Google Map Embed API](google-embed/index.html)
 -  [Google Map JS API](google-js/index.html)
--  [Google Map JS WebGL API](google-js-webgl/index.html)
+-  Google Map JS WebGL API
+    - [India Gate](google-js-webgl/index.html)
+    - [New Parliament House](google-js-webgl/index.html#newph)
--- google-js-webgl/app.js
+++ google-js-webgl/app.js
@@ -1,20 +1,60 @@
 
+
+var hash = window.location.hash || "#ig";
+
+
+var loc;
+var source;
+
+var intialTilt = 0;
+var intialZoom = 17.5;
+var intialHeading;
+var intialScaleY = 1;
+
+var finalTilt = 45;
+var finalZoom = 18;
+var finalHeading;
+var finalScaleY;
+
+if(hash == "#ig") {
+	loc = { lat: 28.6129, lng: 77.2295 };
+	source = "india_gate.glb";
+	intialHeading = 90;
+	finalHeading = intialHeading + 0;
+	intialScaleY = 5;
+	finalScaleY = 5;
+	document.title = "India Gate";
+} else if(hash== "#newph") {
+	loc = { lat: 28.6172, lng: 77.2094 };
+	source = "newph.glb";
+	intialHeading = -90;
+	finalHeading = intialHeading + 90;
+	intialScaleY = 5;
+	finalScaleY = 25;
+	document.title = "New Parliament House";
+}
+
 const mapOptions = {
-  "tilt": 0,
-  "zoom": 18,
-  "heading": 0, // rotation
-  "center": { lat: 28.612952919340955, lng: 77.22956784426097 }
+  "tilt": intialTilt,
+  "zoom": intialZoom,
+  "heading" : intialHeading,
+  "center": loc
 }
 
 async function initMap() {
   const mapDiv = document.getElementById("map");
-  const map = new google.maps.Map(mapDiv, { ...mapOptions, "mapId": "f7880d427f9b40ba", "mapTypeId": "satellite" });
+  const map = new google.maps.Map(mapDiv, { 
+  ...mapOptions, 
+  "mapId": "f7880d427f9b40ba", 
+  "mapTypeId": "hybrid" 
+  //"mapTypeId": "satellite"
+  });
   const view = initWebGLOverlayView(map);
 }
 
 function initWebGLOverlayView(map) {
  
-  let scene, renderer, camera, loader;
+  let scene, renderer, camera, loader, gltfRef;
   const webGLOverlayView = new google.maps.WebGLOverlayView();
   
   webGLOverlayView.onAdd = () => {   
@@ -24,23 +64,27 @@ function initWebGLOverlayView(map) {
 	// set up the scene
     scene = new THREE.Scene();
     
-	const ambientLight = new THREE.AmbientLight( 0xffffff, 0.5 ); // soft white light
+	const ambientLight = new THREE.AmbientLight( 0xffffff, 1 ); // soft white light
     scene.add(ambientLight);
     
-	const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-    directionalLight.position.set(0.5, -1, 0.5);
+	const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(1, 1, 1);
     scene.add(directionalLight);
   
     // load the model    
-    loader = new THREE.GLTFLoader();               
-    const source = "india_gate.glb";
+    loader = new THREE.GLTFLoader();                   
     loader.load(
       source,
-      gltf => {      
-        gltf.scene.scale.set(5, 5, 5);
-        gltf.scene.rotation.x = 90 * Math.PI/180; // rotations are in radians
+      gltf => {
+		gltfRef = gltf;
+		gltf.scene.rotation.x = 90 * Math.PI/180; // rotations are in radians
 		gltf.scene.rotation.y = 90 * Math.PI/180; // rotations are in radians
-        scene.add(gltf.scene);           
+		if(hash == "#ig") {
+			gltf.scene.scale.set(5, intialScaleY, 5);
+		} else if(hash== "#newph") {
+			gltf.scene.scale.set(25, intialScaleY, 25);
+		}
+        scene.add(gltf.scene);
       }
     );
   }
@@ -60,17 +104,32 @@ function initWebGLOverlayView(map) {
       renderer.setAnimationLoop(() => {
 
 		// 2d to 3d 
-		if (mapOptions.tilt < 45) {
-          mapOptions.tilt += 1;
-		} else 
+		if (mapOptions.tilt < finalTilt) {
+          mapOptions.tilt += .25;
+		  // intialScaleY += .25;
+		  // gltfRef.scene.scale.set(25, intialScaleY, 25);
+		  // mapOptions.heading -= .5;
+		} 
 		// zoom in
-		if (mapOptions.zoom < 17.5) {
-		  mapOptions.zoom += 0.05;
-        } else
+		else if (mapOptions.zoom < finalZoom) {
+		  mapOptions.zoom += 0.01;
+        } 
         // rotate the map 360 degrees 
-        if (mapOptions.heading <= 90) {
+        else if (mapOptions.heading <= finalHeading) {
           mapOptions.heading += 0.2;
-        } else 
+		  if(hash == "#ig") {
+			intialScaleY += .01;
+			gltfRef.scene.scale.set(5, intialScaleY, 5);
+		  } else if(hash== "#newph") {
+			intialScaleY += .05;
+			gltfRef.scene.scale.set(25, intialScaleY, 25);
+		  }
+        } 
+		else if (intialScaleY <= finalScaleY) {
+          //intialScaleY += .05;
+		  //gltfRef.scene.scale.set(25, intialScaleY, 25);
+        }
+		else 
 		{
           renderer.setAnimationLoop(null)
         }
